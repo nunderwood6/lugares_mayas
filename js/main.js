23187ccac040a8da77dd5178c1bb32c5e86f3117
Nick Underwood <nunderwood6@gmail.com>
2019-02-14 17:51:46 -0600

    Added proportional symbols

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
+//////Next Steps
+//Change base tileset
+//Add slider and symbolize based on active year
+
+
+
 //keep map object in global scope
 var myMap;
+var symbols;
+var activeYear = "1980";
+
+var topLeft = [46.40756396630067,-79.365234375],
+    botRight = [40.094882122321145,-67.19238281249999];
 
 function initialize(){
 
@@ -19,15 +30,20 @@ function loadJson(){
 		console.log(snowfallData);
 		//add markers
 		addMarkers(snowfallData)
+		sizeMarkers();
 
 	})
 	
 
 }
 
+
+/////////////////////////////////////////////////////////////
+
+
 function addMap(){
 //initialize leaflet map
-myMap = L.map('mapid').setView([43.674753, -72.191216], 6);
+myMap = L.map('mapid').fitBounds([topLeft, botRight]);
 
 //load tile layer
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibnVuZGVyd29vZDYiLCJhIjoiY2o2aDQ5NWN0MDVmcjMybG00Mm9icml4ZSJ9.jgZOCzIY9h-gZnpdsGjiQA",{
@@ -39,17 +55,47 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
 
 }
 
+
+/////////////////////////////////////////////////////////////
+
+
+
 function addMarkers(jsonData){
 
 //add megacities with default circle marker styling
- L.geoJSON(jsonData,{
+  symbols =  L.geoJSON(jsonData,{
  	pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng);
+ 		console.log("here");
+        return L.circleMarker(latlng, {
+        	style: function(feature){
+        		return {
+        			"radius": feature.properties["1980"]
+        		};
+        	}
+        });
     }
  }).addTo(myMap);
 
 
+
 }
+
+function sizeMarkers(){
+
+symbols.eachLayer(function(layer){
+		console.log(layer.feature.properties);
+		var r = Math.sqrt(layer.feature.properties[activeYear]);
+		console.log(r);
+		layer.setStyle({
+			radius: r,
+			weight: 0.5,
+			fillOpacity: 0.8
+		});
+
+});
+
+}
+
 
 
 $(document).ready(initialize);
